3393b1a3343226966917ecd79d33b15bde4e94e2
nnmadalin <52752339+nnmadalin@users.noreply.github.com>
2023-04-10 11:30:21 +0300

    Add files via upload

---
+++ CIA2011judet/CIA_jud_2011_C#/Probleme_Proba_Practica/Solutii/CIA2011Judet/CIA2011Judet/logare.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace CIA2011Judet
+{
+    public partial class logare : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataView dv = new DataView();
+            dv = (DataView)SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            try
+            {
+                Session.Add("utilizator", dv.Table.Rows[0].ItemArray[0].ToString());
+                Session.Add("parola", dv.Table.Rows[0].ItemArray[1].ToString());
+                Session.Add("tip", dv.Table.Rows[0].ItemArray[2].ToString());
+                if (dv.Table.Rows[0].ItemArray[2].ToString() == "admin")
+                    Response.Redirect("admin.aspx");
+                else
+                {
+                    SqlDataSource2.Insert();
+                    Response.Redirect("user.aspx");
+                }
+
+                    
+            }
+            catch {
+                Label1.Text = "Logare nereusita";
+            }
+
+
+                
+            
+
+        }
+
+        protected void Label2_Load(object sender, EventArgs e)
+        {
+            Label2.Text = DateTime.Now.ToLongDateString();
+        }
+    }
+}
---
+++ CIA2011judet/CIA_jud_2011_C#/Probleme_Proba_Practica/Solutii/Messenger/Messenger/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+
+
+
+
+namespace Messenger
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMaria_Click(object sender, EventArgs e)
+        {
+            rtbAfisareMesaj.SelectionColor = Color.Red;
+            string text = "Maria: " + rtbEditareMesaj.Text;
+            rtbEditareMesaj.Text = text+"\n";
+            rtbAfisareMesaj.AppendText(rtbEditareMesaj.Text);
+            rtbEditareMesaj.Focus();
+            rtbEditareMesaj.Text = string.Empty;
+        }
+
+        private void btnIonel_Click(object sender, EventArgs e)
+        {
+            rtbAfisareMesaj.SelectionColor = Color.Blue;
+            string text = "Ionel: " + rtbEditareMesaj.Text;
+            rtbEditareMesaj.Text = text + "\n";
+            rtbAfisareMesaj.AppendText(rtbEditareMesaj.Text);
+            rtbEditareMesaj.Focus();
+            rtbEditareMesaj.Text = string.Empty;
+        }
+
+        private void btnSalvare_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile1 = new SaveFileDialog();
+
+            //Initializari
+            saveFile1.DefaultExt = "*.rtf";
+            saveFile1.Filter = "RTF Files|*.rtf";
+            saveFile1.Title = "Save as...";
+           
+            if (saveFile1.ShowDialog() == DialogResult.OK &&
+               saveFile1.FileName.Length > 0)
+            {
+                rtbAfisareMesaj.SaveFile(saveFile1.FileName);
+            }
+
+        }
+
+        private void btnIncarcare_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile2 = new OpenFileDialog();
+            openFile2.DefaultExt = "*.rtf";
+            openFile2.Filter = "RTF Files|*.rtf";
+            openFile2.Title = "Open";
+                
+            if (openFile2.ShowDialog() == DialogResult.OK)
+            {   
+                StreamReader sr = new StreamReader(openFile2.FileName, Encoding.Default);      
+                string str = sr.ReadToEnd();         
+                sr.Close();
+                rtbAfisareMesaj.SelectedRtf = str;
+            }
+        }
+
+        private void btnSterge_Click(object sender, EventArgs e)
+        {
+            rtbAfisareMesaj.Text="";
+        }
+    }
+}
